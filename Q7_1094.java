56987b5dcff3fc123e0317a1aab96029e61efbd4
Dheeraj Kumar <dheerajkumarn1306@gmail.com>
2023-10-15 21:43:15 +0530

    Q7 completed

---
+++ Q7_1094.java
@@ -0,0 +1,34 @@
+/*
+ * Name: Dheeraj Kumar
+ * Reg no.:2241011127
+ * PS LINK: https://cses.fi/problemset/task/1069/
+ */
+import java.util.Scanner;
+
+public class Q7_1094 {
+    public static void main(String[] args) {
+        long max=1;
+        long curr=1;
+        Scanner sc=new Scanner(System.in);
+        String dna=sc.nextLine();
+        for(int i=0;i<dna.length()-1;++i){
+            if(dna.charAt(i)==dna.charAt(i+1)){
+                curr++;
+
+            }else{
+                if(max<curr){
+                    max=curr;
+                }
+                curr=1;
+            }
+        }
+        if(max<curr){
+            max=curr;
+        }
+        System.out.println(max);
+
+
+    
+}
+    
+}
