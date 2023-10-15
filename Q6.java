e53e1bd39ff0f5fb66380d040c44cb9dac210deb
Dheeraj Kumar <dheerajkumarn1306@gmail.com>
2023-10-15 20:53:23 +0530

    Q6 completed

---
+++ Q6.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Q6 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        long input1=Integer.parseInt(sc.nextLine());
+        long result,combination,count;
+        count=0;
+        for(long i=1;i<=input1;i++){
+            if(i==1){
+                result=0;
+            }else{
+                combination =(i*i)*(i*i-1)/2;
+                result=(combination-8*count);
+                count=count+1-1;
+            }
+            System.out.println(result);
+        }
+
+      }
+    
+}
